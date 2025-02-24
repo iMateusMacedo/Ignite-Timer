6959635875fed7c1267c5b5bce36516c37392a26
Mateus Macedo <105501644+Mateusfdsm1@users.noreply.github.com>
2025-02-24 11:20:28 -0300

    configuração do global.css + themes do styled components

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -18,9 +18,15 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${props => {
+  background-color: ${props => props.theme['green-500']};
+  color: ${props => props.theme.white};
+  border-radius: 4px;
+  border: 0px;
+  margin: 8px;
+
+  /* ${props => {
     return css`
       background-color: ${buttonVariants[props.variant]}}
     `
-  }}
+  }} */
 `
